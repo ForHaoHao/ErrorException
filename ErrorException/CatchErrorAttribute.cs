be7f8c8235e0ae27bd4844a88d8a1259c603ebeb
hao.chen <tcit.hao@outlook.com>
2025-07-31 14:32:24 +0800

    feat/CatchError: Add attribute method,Can catching exception

---
+++ ErrorException/CatchErrorAttribute.cs
@@ -0,0 +1,26 @@
+﻿using ErrorException;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatchError
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Module)]
+    public class CatchErrorAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            if(context.Exception != null)
+            {
+                if (context.Exception is not AppException)
+                {
+
+                    var errorResponse = new AppException(500, "內部伺服器發生錯誤", context.Exception.Message);
+                }
+            }
+        }
+    }
+}
